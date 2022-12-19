d61b1d2edb67effd7c9e58f1d9d5c5d1650df7ee
taikurachi <tk2958@nyu.edu>
2022-12-19 15:58:19 -0500

    final iterations with clouds and win/lose screens

--- classes.js
+++ classes.js
@@ -11,11 +11,29 @@ class Ball {
   }
 
   jump() {
+    //let canJump = true; 
+
     if(this.y == height - 26) { //making sure you can't just fly in the air while spamming the jump key
-  this.velocityY = -20;
+  this.velocityY = - 20;
 }
 
+  //if(this.y <= 500 ) { // tried to experiment with multiple jumps and limiting the timing, but could not get it to work
+    //canJump = false;
+    //setTimeout(function() {
+      //canJump = true; 
+    //}, 1000);
+  //}
+
   }
+
+
+  jump1() {
+    //let canJump = true; 
+
+    if(this.y == height - 26) { //making sure you can't just fly in the air while spamming the jump key
+  this.velocityY = - 30;
+}
+}
 
   hits(obstacles) {
     return collideRectCircle(obstacles.x, obstacles.y, obstacles.r, obstacles.r, this.x, this.y, this.r); //implementation of 2d collisions
@@ -24,11 +42,13 @@ class Ball {
   move() {
     this.y += this.velocityY;
     this.velocityY += this.gravity; //adding gravity for realistic jumping physics
-    this.y = constrain(this.y, 0, height - 26); //constraining the variable so the ball is in frame 
-  }
+
+     this.y = constrain(this.y, 0, height - 26); //constraining the variable so the ball is in frame 
+    }
+  
 
   display() {
-
+    fill(255, 0 ,0 );
     ellipse(this.x, this.y, this.r, this.r);
 
   }
@@ -45,12 +65,50 @@ constructor() {
 }
 
 move() {
-  this.x -= 7; //so the ball looks like it's mocing
+  this.x -= 7; //so the ball looks like it's moving
 }
 
 
 display() {
-  rect(this.x, this.y, this.r, this.r);
+  image(tree, this.x, this.y, this.r, this.r);
 }
 
+}
+
+
+class Flake {
+  
+  constructor(){
+    // initialize coordinates
+    this.posX = 0;
+    this.posY = random(-50, 0);
+    this.initialangle = random(0, 2 * PI);
+    this.size = random(2, 10);
+
+    // radius of snowflake spiral
+    // chosen so the snowflakes are uniformly spread out in area
+    this.radius = sqrt(random(pow(width / 2, 2)));
+    this.color = color(random(255), random(255), random(255));
+  }
+
+  update(time) {
+    // x position follows a circle
+    let w = 0.6; // angular speed
+    let angle = w * time + this.initialangle;
+    this.posX = width / 2 + this.radius * sin(angle);
+
+    // different size flakes fall at slightly different y speeds
+    this.posY += pow(this.size, 0.5);
+
+    // delete flake if past end of screen
+    if (this.posY > height) {
+      let index = flakes.indexOf(this);
+      flakes.splice(index, 1);
+    }
+  };
+
+  display() {
+    fill(this.color);
+    ellipse(this.posX, this.posY, this.size);
+  };
 }
--- sketch.js
+++ sketch.js
@@ -1,23 +1,50 @@
 let ball;
 let obstacles = []; //array of obstacles so I can add to screen randomly later
-let score = 0;
 let screen = 0;
+let cloud1;
+let cloud2;
+let posX = 950;
+let posX2 = 500;
+let posY = 0;
+let score = 0;
+let flakes = [];
+let tree;
+let plane;
+//let canJump = true;
 
+function preload(){
+  cloud1 = loadImage('cloud.png');
+  cloud2 = loadImage('clouds2.png');
+  BG = loadImage('background.jpeg');
+  tree = loadImage('tree.png');
+  plane = loadImage('plane.png');
+}
 
 function setup() {
-  createCanvas(600, 450);
+  createCanvas(1300, 700);
   ball = new Ball();
+  
 
 }
 
 function keyPressed () {
   if (key == ' ') { //using space bar to jump
-    ball.jump(); 
+    ball.jump();
+  } if (key == 'j') {   //using space bar to jump
+    ball.jump1();
+   //canJump = false; 
   }
 }
 
+/*
+function mousePressed (){
 
+if(screen == 3) {
+  reset();
+}
 
+}
+*/
 function draw() {
 
 
@@ -27,53 +54,74 @@ function draw() {
 
   if(screen == 1) {
     game();
+    ALLBG();
   }
 
 if(screen == 2) {
   winScreen();
 }
 
-  if(mouseIsPressed == true){
+if(screen == 3) {
+  loseScreen();
+}
+
+  if(mouseIsPressed == true && screen == 0){
     screen = 1;
   }
-
+/*
+if(keyIsPressed == true && screen == 3){
+    screen = 0;
+  }
+*/
 }
 
 
 
 function instructions (){
 
+  background(255);
+  fill(0);
   textSize(50);
-  text('HOW TO PLAY', width/2 - 160, height/2 - 30);
+  text('HOW TO PLAY', width/2 - 160, height/2 - 65);
   textSize(30);
-  text('Use Space Bar to Jump', width/2 - 160, height/2);
+  text('Press Space Bar to Jump', width/2 - 160, height/2 - 30);
+  text('Press J to Mega Jump', width/2 - 160, height/2);
   textSize(15);
-  text('Click to continue', width/2 - 50, height/2 + 20);
+  text('Click to continue', width/2 - 50, height/2 + 25);
 
 }
 
+function ALLBG (){
+
+ image(cloud1, posX, 100, 100, 100);
+ image(cloud1, posX2 + 2000, 150, 100, 100);
+image(cloud1, posX2 + 1500, 150, 100, 100);
+ image(cloud1, posX2 + 50, 200, 200, 100);
+ image(cloud1, posX2 + 1000, 300, 200, 100);
+ image(cloud2, posX2 + 600, 50, 250, 100);
+ image(plane, posX2 + 1200, 25, 100, 100, 100);
+  posX-=1;
+  posX2-=.5;
+}
 
 function game(){
 
-  background(220);
+
+  background(BG);
   ball.display();
   ball.move();
 
 
-if (random(1)< 0.009) { //creates a random iteation of an obstacle
+if (random(1)< 0.01) { //creates a random iteration of an obstacle
   obstacles.push(new Obstacle()); //reffering back to array
 }
-
-
-
-
 
  
 fill(255);
 stroke(0);
 textSize(30);
-text('SCORE:', 375, 50);
-text(score, 500, 50);
+text('SCORE:', 575, 50);
+text(score, 700, 50);
 
 score += 1; 
 
@@ -82,8 +130,9 @@ score += 1;
     o.move();
   o.display();
   if(ball.hits(o)) { //2D collision implementation using external library
+    screen = 3;
     console.log('game over');
-    noLoop(); //end the game
+    //noLoop(); //end the game
 
   }
   }
@@ -93,14 +142,46 @@ screen = 2;
 
 }
 
-
 }
 
 
 function winScreen (){
 
-textSize(50);
-  text('YOU WIN', width/2 - 160, height/2);
+background(245, 222, 26);
+textSize(80);
+fill(255);
+  text('YOU WIN!', width/2 - 220, height/2);
+  textSize(50);
+  text('refresh to play again', width/2 - 250, height/2 + 50);
 
+  //play at the same time as win screen
+
+let t = frameCount / 60; // update time
+
+  // create a random number of flakes each frame
+  for (let i = 0; i < random(5); i++) {
+    flakes.push(new Flake()); 
+  }
+
+ 
+  for (let flake of flakes) {
+    flake.update(t); 
+    flake.display();
+}
+  
 }
 
+
+
+
+
+function loseScreen (){
+
+
+textSize(50);
+  text('YOU LOSE', width/2 - 160, height/2);
+  textSize(15);
+  text('Refresh to Restart', width/2 - 80, height/2 + 20);
+
+
+}
